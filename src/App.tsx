5555f15f42f1f265ea611c01499896236890e6dc
Jered Danielson <jered@uw.edu>
2023-02-21 21:44:15 -0800

    Initial skeleton validating MUI and React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,146 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MenuIcon from "@mui/icons-material/Menu";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Toolbar from "@mui/material/Toolbar";
+import { i18n } from "@/utils/i18n";
+import { useCallback, useState } from "react";
+import { Link, Route, Routes, useNavigate } from "react-router-dom";
+
+const DRAWER_WIDTH = 320;
+
+function MainPage() {
+  return <Box>Hello, world</Box>;
+}
+
+function OtherPage() {
+  return (
+    <Box>
+      <Box>Goodbye, world</Box>
+      <Box>
+        <Link to="/">Return</Link>
+      </Box>
+    </Box>
+  );
+}
+
+function MenuContents({
+  setIsMobileOpen,
+}: {
+  setIsMobileOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Toolbar>
+        <Button>useremail@gmail.com</Button>
+      </Toolbar>
+      <Divider />
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton
+            onClick={() => {
+              setIsMobileOpen(false);
+              navigate("other");
+            }}
+          >
+            <ListItemText primary={i18n.gettext("Other Page")} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </>
+  );
+}
 
 function App() {
+  const [isMobileOpen, setIsMobileOpen] = useState(false);
+
+  const handleDrawerToggle = useCallback(() => {
+    setIsMobileOpen((prev) => !prev);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${DRAWER_WIDTH}px)` },
+          ml: { sm: `${DRAWER_WIDTH}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: DRAWER_WIDTH }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        <Drawer
+          variant="temporary"
+          open={isMobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: DRAWER_WIDTH,
+            },
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <MenuContents setIsMobileOpen={setIsMobileOpen} />
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: DRAWER_WIDTH,
+            },
+          }}
+          open
+        >
+          <MenuContents setIsMobileOpen={setIsMobileOpen} />
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${DRAWER_WIDTH}px)` },
+        }}
+      >
+        <Toolbar />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/other" element={<OtherPage />} />
+        </Routes>
+      </Box>
+    </Box>
   );
 }
 
---
+++ src/utils/i18n/index.tsx
@@ -0,0 +1,3 @@
+import baseI18n from "gettext.js";
+
+export const i18n = baseI18n();
